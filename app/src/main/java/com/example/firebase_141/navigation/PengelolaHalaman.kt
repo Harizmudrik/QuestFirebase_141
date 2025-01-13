e8ad1acf55fe56d0c894b024f362756f47184b59
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-01-13 12:01:22 +0700

    menambahkan fungsi utama yang bertanggung jawab untuk mengatur navigasi antar layar

---
+++ app/src/main/java/com/example/firebase_141/navigation/PengelolaHalaman.kt
@@ -0,0 +1,14 @@
+package com.example.firebase_141.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import java.lang.reflect.Modifier
+
+@Composable
+fun PengelolaHalaman (
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+
+}
