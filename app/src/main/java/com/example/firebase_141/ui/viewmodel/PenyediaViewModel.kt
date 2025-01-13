f3ad610eb3e34c5ad08e46933cce3d93d30c7827
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-01-13 11:28:35 +0700

    mendaftarkan ViewModel di mahasiswa View Model

--- app/src/main/java/com/example/firebase_141/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/firebase_141/ui/viewmodel/PenyediaViewModel.kt
@@ -9,7 +9,7 @@ import com.example.firebase_141.MahasiswaApp
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(MahasiswaApp().containerApp.repositoryMhs)}
-
+        initializer { InsertViewModel(MahasiswaApp().containerApp.repositoryMhs)}
     }
 }
 
