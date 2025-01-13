5c80f697a00e3f7c15abdd5adb1cfdd0f532c3d0
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-01-13 11:45:24 +0700

    membuat pilihan radio button untuk jenis kelamin

--- app/src/main/java/com/example/firebase_141/ui/pages/InsertView.kt
+++ app/src/main/java/com/example/firebase_141/ui/pages/InsertView.kt
@@ -1,13 +1,17 @@
 package com.example.firebase_141.ui.pages
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
@@ -56,5 +60,26 @@ fun FormMahasiswa (
             color = Color.Red
         )
         Spacer(modifier = Modifier.height(16.dp))
+        Text("Jenis Kelamin")
+        Row (
+            modifier = Modifier.fillMaxWidth()
+        ){
+            gender.forEach { item ->
+                Row (
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ){
+                    RadioButton(
+                        selected = mahasiswaEvent.gender == item,
+                        onClick = { onValueChange(mahasiswaEvent.copy(gender = item)) }
+                    )
+                    Text(text = item)
+                }
+            }
+        }
+        Text (
+            text = errorState.gender ?: "",
+            color = Color.Red
+        )
     }
 }
