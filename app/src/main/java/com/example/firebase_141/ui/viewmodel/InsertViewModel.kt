8c1ec14f39073a0960db2bcb0ec612b2c1bd6519
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-01-13 11:21:52 +0700

    menambahkan fungsi untuk menyimpan status error dari input pengguna.

--- app/src/main/java/com/example/firebase_141/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebase_141/ui/viewmodel/InsertViewModel.kt
@@ -72,3 +72,16 @@ data class InsertUiState(
     val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
     val isEntryValid: FormErrorState = FormErrorState()
 )
+
+data class FormErrorState (
+    val nim: String? = null,
+    val nama: String? = null,
+    val gender: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null
+) {
+    fun isValid(): Boolean {
+        return nim == null && nama == null && gender == null && alamat == null && kelas == null && angkatan == null
+    }
+}
