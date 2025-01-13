e38b77a583864b0e0418e50b1f472a6b04ae876c
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-01-13 11:16:50 +0700

    menambahkan fungsi ini memeriksa apakah semua input valid; Jika ada input kosong, error message akan diberikan

--- app/src/main/java/com/example/firebase_141/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebase_141/ui/viewmodel/InsertViewModel.kt
@@ -18,4 +18,18 @@ class InsertViewModel (
     fun updateUiEvent(event: MahasiswaEvent) {
         uiEvent = uiEvent.copy(insertUiEvent = event)
     }
+
+    fun validateFields (): Boolean {
+        val event = uiEvent.insertUiEvent
+        val errorState = FormErrorState(
+            nim = if (event.nim.isEmpty()) "NIM tidak boleh kosong" else null,
+            nama = if (event.nama.isEmpty()) "Nama tidak boleh kosong" else null,
+            gender = if (event.gender.isEmpty()) "Jenis Kelamin tidak boleh kosong" else null,
+            alamat = if (event.alamat.isEmpty()) "Alamat tidak boleh kosong" else null,
+            kelas = if (event.kelas.isEmpty()) "Kelas tidak boleh kosong" else null,
+            angkatan = if (event.angkatan.isEmpty()) "Angkatan tidak boleh kosong" else null
+        )
+        uiEvent = uiEvent.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
 }
