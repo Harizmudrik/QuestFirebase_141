d9083966c0495d5bb1935a81c5361e99acfa40ae
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-01-13 11:12:24 +0700

    menambahkan class yang mengatur logika aplikasi

--- app/src/main/java/com/example/firebase_141/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebase_141/ui/viewmodel/InsertViewModel.kt
@@ -1,2 +1,8 @@
 package com.example.firebase_141.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import com.example.firebase_141.repository.RepositoryMhs
+
+class InsertViewModel (
+    private val mhs: RepositoryMhs
+): ViewModel()
