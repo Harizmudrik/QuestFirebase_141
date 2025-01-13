dc1901cbb52288089c3356ffbd54c201bfbb5f3c
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-01-13 11:42:58 +0700

    Membuat form input untuk NIM

--- app/src/main/java/com/example/firebase_141/ui/pages/InsertView.kt
+++ app/src/main/java/com/example/firebase_141/ui/pages/InsertView.kt
@@ -2,11 +2,13 @@ package com.example.firebase_141.ui.pages
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import com.example.firebase_141.ui.viewmodel.FormErrorState
 import com.example.firebase_141.ui.viewmodel.MahasiswaEvent
 
@@ -34,6 +36,20 @@ fun FormMahasiswa (
         )
         Text(
             text = errorState.nama ?: "",
+            color = Color.Red
+        )
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = mahasiswaEvent.nim,
+            onValueChange = { onValueChange(mahasiswaEvent.copy(nim = it)) },
+            label = { Text("NIM") },
+            isError = errorState.nim != null,
+            placeholder = { Text("Masukkan NIM") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Text (
+            text = errorState.nim ?: "",
             color = Color.Red
         )
     }
