2842f293e444b81cadcf3f4efeda6bb56c96670f
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-01-13 12:03:17 +0700

    menambahkan struktur utama navigasi dan rute yang mengelola antar layar.

--- app/src/main/java/com/example/firebase_141/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/firebase_141/navigation/PengelolaHalaman.kt
@@ -2,7 +2,10 @@ package com.example.firebase_141.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import com.example.firebase_141.ui.pages.HomeScreen
+import com.example.firebase_141.ui.pages.InsertMhsView
 import java.lang.reflect.Modifier
 
 @Composable
@@ -10,5 +13,22 @@ fun PengelolaHalaman (
     modifier: Modifier,
     navController: NavHostController = rememberNavController()
 ) {
-
+    NavHost (
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    ){
+        composable(route = DestinasiHome.route) {
+            HomeScreen (
+                navigateToltemEntry = { navController.navigate(DestinasiInsert.route) }
+            )
+        }
+        composable(route = DestinasiInsert.route) {
+            InsertMhsView(
+                onBack = { navController.popBackStack() },
+                onNavigate = {
+                    navController.navigate(DestinasiHome.route) }
+            )
+        }
+    }
 }
