5eb5de3592b5ba1dc44b329ceac42411bbddcbaa
Harizmudrik <muhammadharizmudrik@gmail.com>
2025-01-13 11:58:14 +0700

    menambahkan object yang mengimplementasikan DestinasiNavigasi

--- app/src/main/java/com/example/firebase_141/navigation/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/firebase_141/navigation/DestinasiNavigasi.kt
@@ -5,3 +5,8 @@ interface DestinasiNavigasi {
     val route: String
     val titleRes: String
 }
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home"
+}
